d7bff76295e81fabebb1b2fa29d0cb7b2c0c3b18
Carl Lewis Castillo <carllewiscastillo@gmail.com>
2022-06-12 10:47:26 -0400

    Initial Commit

---
+++ HotelBooking/HotelBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBookingApp.swift
+//  HotelBooking
+//
+//  Created by macbook on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotelBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
